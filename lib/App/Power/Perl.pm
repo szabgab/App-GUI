b8beaa87b26268bcdba211233c666408cf0e6219
Gabor Szabo <gabor@szabgab.com>
2013-09-04 09:58:30 +0300

    select file, read an display it

--- lib/App/Power/Perl.pm
+++ lib/App/Power/Perl.pm
@@ -2,10 +2,19 @@ package App::Power::Perl;
 use 5.010;
 use Moo;
 
-use Prima qw(Application Buttons InputLine Label MsgBox);
+use Prima qw(Application Buttons InputLine Label MsgBox FileDialog Edit);
 
 our $VERSION = 0.01;
 
+has output => (is => 'rw');
+has code   => (is => 'rw', default => sub { {} } );
+
+my $welcome = <<"END_WELCOME";
+Welcome to the Power Perl v$VERSION
+
+Select a text file using Editor/Select file
+and the press the "Run" button.
+END_WELCOME
 
 sub run {
 	my ($self) = @_;
@@ -16,9 +25,13 @@ sub run {
 					[ '~Exit', 'Alt-X', '@X', sub { exit } ],
 				],
 			],
+			[ '~Editor' => [
+					[ 'Select File', sub { $self->select_file(@_) } ],
+				],
+			],
 			[],
 			[ '~Help' => [
-					[ '~About', \&about ],
+					[ '~About', \&show_about ],
 				],	
 			],
 		],
@@ -26,42 +39,103 @@ sub run {
 		size   => [400, 400],
 		#origin => [0, 100],  # left, bottom, at least on OSX it defaults to the left top corner
 	);
+
+	$self->output( $w->insert( Edit =>
+		pack => { fill => 'both', expand => 1, },
+		readOnly => 1,
+		text => $welcome,
+	));
 	
-	$w->insert( Label =>
-		text   => 'URL',
-		origin => [0, 300],
-	);
-	
-	my $input = $w->insert( InputLine =>
-		text        => '',
-		origin      => [50, 300],
-		#centered    => 1,
-		width       => 300,
-	#	firstChar   => 10,
-		#alignment   => ta::Center,
-		#font        => { size => 18, },
-		#growMode    => gm::GrowHiX,
-		#buffered    => 1,
-		borderWidth => 3,
-		#autoSelect  => 0,
-	);
-	
+#	$w->insert( Label =>
+#		text   => 'URL',
+#		origin => [0, 300],
+#	);
+#	
+#	my $input = $w->insert( InputLine =>
+#		text        => '',
+#		origin      => [50, 300],
+#		#centered    => 1,
+#		width       => 300,
+#	#	firstChar   => 10,
+#		#alignment   => ta::Center,
+#		#font        => { size => 18, },
+#		#growMode    => gm::GrowHiX,
+#		#buffered    => 1,
+#		borderWidth => 3,
+#		#autoSelect  => 0,
+#	);
 	
 	my $btn = $w->insert( Button =>
-		origin   => [50,180],
+		origin   => [0, 0],
 		text     => 'Run', 
 		pressed  => 0,
-		onClick  => sub {
-			my $self = shift;
-			#say 'Button pressed ' . $input->text;
-		},
+		onClick  => sub { $self->run_pressed(@_) },
 	);
 	
 	Prima->run;
 }
 
-sub about {
-	Prima::MsgBox::message_box( 'About Power Perl', "Power Perl v$VERSION\nHacked together by Gabor Szabo", mb::Ok);
+sub select_file {
+	my ($self, $main, $c) = @_;
+
+	my $open = Prima::OpenDialog-> new(
+		text => 'Select a file',   # the title of the window
+		filter => [
+			['All' => '*'],
+			['Perl modules' => '*.pm'],
+		],
+		# TODO: The button should not read 'Open' but 'Select'
+	);
+
+	# Experiement to creat a button that is like an OpenDialog but
+	# say 'Select' instead of 'Open'
+	#my $open = Prima::FileDialog-> new(
+	#	text => 'Select a file',   # the title of the window
+	#	openMode => 1,
+	#	filter => [
+	#		['Perl modules' => '*.pm'],
+	#		['All' => '*']
+	#	],
+	#	multiSelect => 1,
+	#);
+	if ($open->execute) {
+		#say "File selected " . $open->fileName;
+		$self->code->{file} = $open->fileName;
+	}
+}
+
+
+sub show_about {
+	my ($main, $c) = @_;
+	Prima::MsgBox::message_box( 'About Power Perl',
+		"Power Perl v$VERSION\nHacked together by Gabor Szabo in 2013.", mb::Ok);
+}
+
+sub run_pressed {
+	my ($self, $button) = @_;
+
+	my $code = $self->code;
+	if ($code->{file}) {
+		if (open my $fh, '<', $code->{file}) {
+			my $output = $self->output;
+			$output->text('');
+			# TODO: Async read?
+			while (my $line = <$fh>) {
+				$output->insert_text($line . "\n"); # TODO why do we have to add extra newlines?
+			}
+			close $fh;
+		} else {
+			$self->_error("Could not open file '%s'. Error: '%s'", $code->{file}, $!);
+		}
+	} else {
+		$self->_error("No file selected");
+	}
+}
+
+sub _error {
+	my ($self, $format, @args) = @_;
+	my $msg = sprintf($format, @args);
+	say $msg;
 }
 
 1;
